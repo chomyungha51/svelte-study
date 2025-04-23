b142441c1d4d068e635c4d22c5d76a4a0642d546
Myungha Cho <mhcho@in4ucloud.com>
2025-04-23 16:56:57 +0900

    🔥 [ 1일차 ] runes 공부

---
+++ src/lib/MyComponent.svelte
@@ -0,0 +1,27 @@
+<script>
+    /** @type {{ adjective: string, count: number, object:{count:number} }} */
+    let {adjective = 'awesome', count = $bindable(), object} = $props();
+    // object가 일반 객체면 자식에서 바꿔도 다시 렌더링되지 않는 이상 안 바뀌는데, state 객체면 바로 바뀐다. 하지만 이렇게 쓰지 마라
+    // 그냥 state값을 넘기면 일시적으로 자식 범위에서 업데이트할 수 있지만 다시 렌더링되면 부모에서 내려준 값으로 보임
+
+</script>
+
+<p>this component is {adjective}</p>
+
+<button onclick={()=>count++}>cilcks (child): {count} +</button>
+ 
+<button onclick={() => {
+	// has no effect
+	object.count += 1
+}}>
+	click object count : {object.count}
+</button>
+
+<input bind:value={count}/>
+
+<style>
+	input {
+		font-family: 'Comic Sans MS';
+		color: deeppink;
+	}
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,39 @@
+<script>
+  import MyComponent from '../lib/MyComponent.svelte'
+
+  // Runes 종류
+
+  // 1. $state
+  // 단일 값을 state에 넣어서 props로 줄 땐, child에서 임시로 덮어씀
+  let count = $state(0);
+
+  // 2. $derived
+  let doubled = $derived( count * 2);
+
+  let numbers = $state([1,2,3]);
+  let total = $derived.by(() =>{
+    let total = 0;
+    for (const n of numbers) {
+        total += n;
+    }
+    return total;
+  })
+
+  // 3. $props
+  let object = $state({count: 0})
+  
+  // 4 $bindable 로 선언한 props는 넘겨줄 때도 bind:붙여서 줄 수 있음
+
+</script>
+
 <h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+
+<button onclick={() => count++}>clicks (parent) +</button>
+<p>Parent Count: {count}</p>
+
+<p>Double: {doubled}</p>
+<button onclick={() => numbers.push(numbers.length + 1)}>
+	add number {numbers.join(' + ')} = {total}
+</button>
+
+<MyComponent adjective="nice" bind:count={count} object={{count:0}}/>
